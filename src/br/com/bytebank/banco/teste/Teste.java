002fb5218a5a5342e0b59c849007b49277cbd2fd
Marcio Felipe Queiroga <marciofelipequeiroga@gmail.com>
2019-12-23 15:38:04 -0300

    Object

--- src/br/com/bytebank/banco/teste/Teste.java
+++ src/br/com/bytebank/banco/teste/Teste.java
@@ -5,11 +5,17 @@ import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class Teste {
     public static void main(String[] args) {
+//        String dado = "040.969.123-28";
+
         Object cc = new ContaCorrente(69,123);
         Object cp = new ContaPoupanca(123,196);
 
         System.out.println(cc);
         System.out.println(cp);
+
+//        dado = dado.replaceAll("[^0-9]+", "");
+
+//        System.out.println(dado);
 
     }
 }
