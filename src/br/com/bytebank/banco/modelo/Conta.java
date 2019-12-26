bd9a9b39f4f646802ad00f61bea0a85ad369029b
Marcio Felipe Queiroga <marciofelipequeiroga@gmail.com>
2019-12-26 16:20:22 -0300

    ArrayList - equals

--- src/br/com/bytebank/banco/modelo/Conta.java
+++ src/br/com/bytebank/banco/modelo/Conta.java
@@ -76,8 +76,31 @@ public abstract class Conta {
         return titular;
     }
 
+//    public boolean ehIgual(Conta outra){
+//        if (this.agencia != outra.agencia){
+//            return false;
+//        }
+//        if (this.numero != outra.numero){
+//            return false;
+//        }
+//        return true;
+//    }
+
     @Override
     public String toString() {
         return "Numero : " + this.numero + " Agencia : " + this.agencia;
+    }
+
+    @Override
+    public boolean equals(Object ref) {
+        Conta outra = (Conta) ref;
+
+        if (this.agencia != outra.agencia){
+            return false;
+        }
+        if (this.numero != outra.numero){
+            return false;
+        }
+        return true;
     }
 }
